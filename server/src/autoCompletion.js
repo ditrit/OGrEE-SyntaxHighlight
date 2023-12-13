c587e1ad3a17e477762d3141742be443f11e4846
Dahan13 <corentincaugant2001@gmail.com>
2023-12-13 16:08:58 +0100

    Adds variables & structures autocompletion

--- server/src/autoCompletion.js
+++ server/src/autoCompletion.js
@@ -8,15 +8,13 @@ const commandList = require("./../data/command_list.json");
 //need to parse commmand list for easier use during document parsing
 
 export function autoCompletion(documents){
-	return (params) => {
+	return (_textDocumentPosition) => {
 		// The pass parameter contains the position of the text document in
 		// which code complete got requested. For the example we ignore this
 		// info and always provide the same completion items.
-		const document = documents.get(params.textDocument.uri);
+		const document = documents.get(_textDocumentPosition.textDocument.uri);
 		var listCommands = [];
-		var listVariables = getExistingVariables(_textDocumentPosition.position.character);
-		console.log("Position document:", _textDocumentPosition.textDocument.indexAt())
-		console.log("Liste de variables :", listVariables)
+		var listVariables = getExistingVariables(document.offsetAt(_textDocumentPosition.position));
 		listVariables[0].forEach((variable) => {
 				listCommands.push({
 				label: variable,
@@ -67,7 +65,6 @@ export function autoCompletion(documents){
 				});
 			})
 		});
-		console.log("Liste de commandes", listCommands[0])
 		return listCommands;
 	};
 
